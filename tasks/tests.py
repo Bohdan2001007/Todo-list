7d05fca8c925db8f6ae1a7317575b6ed8d961a08
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-08 17:57:33 +0100

    Added tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.urls import reverse
+from datetime import datetime
+from tasks.models import Tag, Task
 
-# Create your tests here.
+
+TASK_URL = reverse("tasks:TaskListView")
+TAG_URL = reverse("tasks:TagListView")
+
+
+class ModelsTests(TestCase):
+    def test_tag_str(self):
+        tag = Tag.objects.create(name="Test")
+
+        self.assertEqual(
+            str(tag),
+            f"{tag.name}",
+        )
+
+
+class DataTasksTests(TestCase):
+    def test_get_tasks_data(self):
+        response = self.client.get(TASK_URL)
+
+        self.assertEqual(response.status_code, 200)
+
+
+class CreateTaskTests(TestCase):
+    def test_create_task(self):
+        self.task = Task.objects.create(
+            content="test",
+            datetime=datetime.now(),
+        )
+        response = self.client.get(TASK_URL)
+
+        all_tasks = Task.objects.all()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual((list(all_tasks)), list(all_tasks))
+
+
+class DataTagsTests(TestCase):
+    def test_get_tags_data(self):
+        response = self.client.get(TAG_URL)
+
+        self.assertEqual(response.status_code, 200)
+
+
+class CreateTagTests(TestCase):
+    def test_create_tag(self):
+        self.tag = Tag.objects.create(
+            name="test",
+        )
+        response = self.client.get(TAG_URL)
+
+        all_tags = Tag.objects.all()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(list(all_tags), list(all_tags))
